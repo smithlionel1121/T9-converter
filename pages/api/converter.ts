0ecaf76ba30c6fa2841fb697c4058634f3fa6d33
Lionel Smith <smithlionel1121@gmail.com>
2021-10-29 23:59:41 +0100

    Configure api to send suggestions from dictionary

--- pages/api/converter.ts
+++ pages/api/converter.ts
@@ -1,9 +1,30 @@
+import { once } from 'events';
+import { createReadStream } from 'fs';
 import ConverterResponse from 'interfaces/ConverterResponse';
+import Trie from 'lib/Trie';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { createInterface } from 'readline';
 
-export default (
+export default async (
   req: NextApiRequest,
   res: NextApiResponse<ConverterResponse>,
 ) => {
-  res.status(200).json({ suggestions: ['do', 'doer', 'doers', 'done'] });
+  const trie = new Trie();
+
+  const {
+    body: { code: numericCode },
+  } = req;
+
+  const file = createInterface({
+    input: createReadStream('lib/data/dictionary.txt'),
+    output: process.stdout,
+    terminal: false,
+  });
+
+  file.on('line', (word) => trie.insertWord(word));
+  await once(file, 'close');
+
+  trie.searchWord(numericCode);
+
+  res.status(200).json({ suggestions: trie.suggestions });
 };
