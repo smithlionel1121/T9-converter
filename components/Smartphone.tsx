344c9d095e6671037bdf7428347be2a228323f92
Lionel Smith <smithlionel1121@gmail.com>
2021-10-30 19:20:50 +0100

    Add smartphone component

---
+++ components/Smartphone.tsx
@@ -0,0 +1,7 @@
+import styles from './Smartphone.module.scss';
+
+const Smartphone = () => {
+  return <div className={styles.smartphone} />;
+};
+
+export default Smartphone;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import Form from 'components/Form';
+import Smartphone from 'components/Smartphone';
 
 const Home = () => {
-  return <Form />;
+  return (
+    <main>
+      <Smartphone />
+    </main>
+  );
 };
 
 export default Home;
