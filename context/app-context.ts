e7edac78c98c4d8461b0cd76dcd0268f0a3686b7
Lionel Smith <smithlionel1121@gmail.com>
2021-10-31 18:20:50 +0000

    Refactor initialization of context

--- context/app-context.ts
+++ context/app-context.ts
@@ -1,6 +1,7 @@
 import AppContextState from 'interfaces/AppContextState';
 import { createContext } from 'react';
+import initialState from './app-initial';
 
-const AppContext = createContext<AppContextState | undefined>(undefined);
+const AppContext = createContext<AppContextState>(initialState);
 
 export default AppContext;
---
+++ context/app-initial.ts
@@ -0,0 +1,7 @@
+import AppContextState from 'interfaces/AppContextState';
+
+const initialState: AppContextState = {
+  numericCode: '',
+};
+
+export default initialState;
