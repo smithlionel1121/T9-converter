6df9f0b293d1e9b56017a1605969953e7b58ebb7
Lionel Smith <smithlionel1121@gmail.com>
2021-11-01 17:07:46 +0000

    Implement debounce for suggestions search

--- components/AppContextProvider.tsx
+++ components/AppContextProvider.tsx
@@ -1,11 +1,13 @@
 import AppContext from 'context/app-context';
 import axios from 'axios';
-import { ReactNode, useEffect, useReducer } from 'react';
+import { ReactNode, useEffect, useMemo, useReducer } from 'react';
 import reducer from 'context/app-reducer';
 import initialState from 'context/app-initial';
 import { ActionType } from 'context/app-actions';
 import ConverterResponse from 'interfaces/ConverterResponse';
+import debounce from 'lib/utils/debounce';
 import AppContextState from 'interfaces/AppContextState';
+import { SUGGESTIONS_DEBOUNCE } from 'lib/constants';
 
 interface Props {
   children: ReactNode;
@@ -14,22 +16,32 @@ interface Props {
 const AppContextProvider = ({ children }: Props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    const getSuggestions = async (
-      numericCode: AppContextState['numericCode'],
-    ) => {
-      const res = await axios.post<ConverterResponse>('/api/converter', {
-        numericCode,
-      });
-      const { suggestions } = res.data;
-      dispatch({
-        type: ActionType.GET_SUGGESTIONS,
-        payload: suggestions,
-      });
-    };
+  const getSuggestions = async (
+    numericCode: AppContextState['numericCode'],
+  ) => {
+    const res = await axios.post<ConverterResponse>('/api/converter', {
+      numericCode,
+    });
+    const { suggestions } = res.data;
+    dispatch({
+      type: ActionType.GET_SUGGESTIONS,
+      payload: suggestions,
+    });
+  };
 
-    getSuggestions(state.numericCode);
-  }, [state.numericCode]);
+  const searchSuggestions = useMemo(
+    () =>
+      debounce(
+        (numericCode: AppContextState['numericCode']) =>
+          getSuggestions(numericCode),
+        SUGGESTIONS_DEBOUNCE,
+      ),
+    [],
+  );
+
+  useEffect(() => {
+    searchSuggestions(state.numericCode);
+  }, [state.numericCode, searchSuggestions]);
 
   const addNumber = (number: number) => {
     if (number > 1)
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const SUGGESTIONS_TARGET = 5;
+export const SUGGESTIONS_DEBOUNCE = 200;
 
 export default SUGGESTIONS_TARGET;
